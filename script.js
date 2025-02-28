3c66b4cf94a6fe11e398a7ed0ee5eb3d0d295a5d
wence25 <wencewence25@gmai.com>
2025-02-28 07:41:48 +0100

    script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sélectionne l'image avec l'ID "photo-carmel"
+const photo = document.getElementById('photo-carmel');
+
+// Ajoute un événement "click" sur l'image
+photo.addEventListener('click', function() {
+    alert('CARMEL JE T\'AIME. TU ES UNE PERSONNE AU GRAND CŒUR ET LE CRÉATEUR SOIT AVEC TOI PUIS T ASSISTE AVEC AMOUR. MAMAN MARIE TE COMBLE DE SA GRÂCE.');
+});
